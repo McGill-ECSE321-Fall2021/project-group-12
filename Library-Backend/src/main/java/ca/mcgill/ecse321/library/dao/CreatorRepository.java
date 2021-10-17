4b67ad5e8cbd89e1e1a4667445b215b960f4fdd4
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2021-10-17 17:30:13 -0400

    Fixed error in CreatorRepository.java

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/dao/CreatorRepository.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/dao/CreatorRepository.java
@@ -1,10 +1,13 @@
 package ca.mcgill.ecse321.library.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 
 import ca.mcgill.ecse321.library.model.Creator;
 import ca.mcgill.ecse321.library.model.Item;
 
+@Repository
 public interface CreatorRepository extends CrudRepository<Creator, Long>{
 	
 	Creator findCreatorByCreatorId(Long creatorId);
