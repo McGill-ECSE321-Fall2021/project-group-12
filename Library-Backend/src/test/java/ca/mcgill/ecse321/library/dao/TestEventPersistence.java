446bf7f57549c85e586182cd0253d1b494aca3ea
LuccaDiLullo <lucca.dilullo@mail.mcgill.ca>
2021-10-18 20:49:43 -0400

    13/13 Passed tests

--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestEventPersistence.java
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestEventPersistence.java
@@ -24,60 +24,24 @@ public class TestEventPersistence {
 	
 	@Autowired
 	private EventRepository eventRepository;
-	@Autowired
-	private LibrarianRepository librarianRepository;
-	@Autowired
-	private TimeSlotRepository timeSlotRepository;
 	
 	@AfterEach
 	public void clearDatabase() {
 		eventRepository.deleteAll();
-		librarianRepository.deleteAll();
-		timeSlotRepository.deleteAll();
 	}
 	
 	@Test
 	public void testPersistAndLoadEvent() {
 		LibraryApplicationSystem system = new LibraryApplicationSystem();
-		String firstName = "Parker";
-		String lastName = "Peter";
-		String address = "123 Street";
-		String username = "TestLibrarian";
-		String password = "11454";
-		String email = "Peter@gmail.com";
-		boolean isLocal = true;
-		Librarian librarian = new Librarian();
-		librarian.setFirstName(firstName);
-		librarian.setLastName(lastName);
-		librarian.setAddress(address);
-		librarian.setUsername(username);
-		librarian.setPassword(password);
-		librarian.setIsLocal(isLocal);
-		librarian.setEmail(email);
-		librarian.setLibraryApplicationSystem(system);
-		librarianRepository.save(librarian);
-		
-		String name = "ECSE321 Event";
-		boolean isPrivate = true;
-		
-		TimeSlot timeSlot = new TimeSlot();
-		Time startTime = Time.valueOf("09:00:00");
-		Time endTime = Time.valueOf("17:00:00");
-		Date startDate = Date.valueOf("2021-10-31");
-		Date endDate = Date.valueOf("2021-11-01");
-		timeSlot.setStartTime(startTime);
-		timeSlot.setEndTime(endTime);
-		timeSlot.setStartDate(startDate);
-		timeSlot.setEndDate(endDate);
-		timeSlot.setLibraryApplicationSystem(system);
-		timeSlotRepository.save(timeSlot);
-		
 		Event event = new Event();
+		
+		String name = "Party";
+		boolean isPrivate = false;
+		
 		event.setName(name);
 		event.setIsPrivate(isPrivate);
-		event.setTimeSlot(timeSlot);
-		event.setUser(librarian);
 		event.setLibraryApplicationSystem(system);
+		
 		eventRepository.save(event);
 		Long id = event.getEventId();
 		
@@ -87,8 +51,6 @@ public class TestEventPersistence {
 		assertNotNull(event);
 		assertEquals(name, event.getName());
 		assertEquals(isPrivate, event.getIsPrivate());
-		assertNotNull(event.getTimeSlot());
-		assertNotNull(event.getUser());
 		assertNotNull(event.getLibraryApplicationSystem());
 		
 	}
--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestReservationPersistence.java
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestReservationPersistence.java
@@ -21,7 +21,6 @@ public class TestReservationPersistence {
 
 	@AfterEach
 	public void clearDatabase() {
-		
 		reservationRepository.deleteAll();
 	}
 	
