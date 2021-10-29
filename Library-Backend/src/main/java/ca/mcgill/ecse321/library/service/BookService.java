597b6b9bf1a8f44edd62e72e66ce2333bf54c53e
LuccaDiLullo <lucca.dilullo@mail.mcgill.ca>
2021-10-29 19:21:21 -0400

    added album and book service

---
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/BookService.java
@@ -0,0 +1,91 @@
+package ca.mcgill.ecse321.library.service;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.library.dao.BookRepository;
+import ca.mcgill.ecse321.library.dao.CreatorRepository;
+import ca.mcgill.ecse321.library.model.Book;
+import ca.mcgill.ecse321.library.model.Book.BMGenre;
+import ca.mcgill.ecse321.library.model.Item;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	BookRepository bookRepository;
+	
+	@Transactional
+	public Book createBook(String title, boolean isArchive, boolean isReservable, Date releaseDate, int numPages, boolean available, BMGenre genre) throws IllegalArgumentException {
+		if (title == null || releaseDate == null) {
+			throw new IllegalArgumentException("Cannot create book with empty fields.");
+		}
+		if (title  == "") {
+			throw new IllegalArgumentException("Cannot create book with empty title.");
+		}
+		boolean valid = false;
+		for (int i=0; i<title.length();i++) {
+			if (title.charAt(i) != ' ') {
+				valid = true;
+				break;
+			}
+		}
+		
+		if(numPages == 0) {
+			valid = false;
+		}
+		
+		if (!valid) {
+			throw new IllegalArgumentException("Cannot create book");
+		}
+		
+		Book book = new Book();
+		book.setTitle(title);
+		book.setIsArchive(isArchive);
+		book.setIsReservable(isReservable);
+		book.setReleaseDate(releaseDate);
+		book.setNumPages(numPages);
+		book.setIsAvailable(available);
+		book.setGenre(genre);
+		return book;	
+	}
+	
+	@Transactional
+	public Book deleteBook(Long bookId) throws IllegalArgumentException {
+		Book book = bookRepository.findBookByItemId(bookId);
+		if (book == null) {
+			throw new IllegalArgumentException("Book does not exist.");
+		}
+		bookRepository.delete(book);
+		return book;
+		
+	}
+	
+	@Transactional
+	public Book getBook(Long bookId) {
+		Book book = bookRepository.findBookByItemId(bookId);
+		if (book == null) {
+			throw new IllegalArgumentException("Book does not exist.");
+		}
+		return book;
+	}
+	
+	@Transactional
+	public List<Book> getAllBooks(){
+		return toList(bookRepository.findAll());
+	}
+	
+	public List<Book> toList(Iterable<Book> iterable){
+		List<Book> books = new ArrayList<Book>();
+		for (Book b:iterable) {
+			books.add(b);
+		}
+		return books;
+	}
+
+}
