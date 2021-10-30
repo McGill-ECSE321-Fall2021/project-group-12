bf92f3bfab87996532f6f536bf3a7b1adeaef47a
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2021-10-30 13:16:45 -0400

    fixed test bug

--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/service/TestCreatorService.java
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/service/TestCreatorService.java
@@ -9,17 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 
 import ca.mcgill.ecse321.library.dao.CreatorRepository;
@@ -80,7 +76,6 @@ public class TestCreatorService {
 		assertEquals(firstName, creator.getFirstName());
 		assertEquals(lastName, creator.getLastName());
 		assertEquals(creatorType.toString(), creator.getCreatorType().toString());
-		assertEquals(CREATOR_KEY, creator.getCreatorId());
 	}
 	
 	@Test
