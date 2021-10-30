bdb01588abfed392ccc9102f6c3d4485e7a1d752
LuccaDiLullo <lucca.dilullo@mail.mcgill.ca>
2021-10-29 20:05:04 -0400

    Added album and book DTOs

---
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/controller/AlbumRestController.java
@@ -0,0 +1,30 @@
+package ca.mcgill.ecse321.library.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.library.service.AlbumService;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+public class AlbumRestController {
+	
+	@Autowired
+	private AlbumService service;
+	
+	
+
+}
