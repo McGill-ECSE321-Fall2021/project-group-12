0b6f7c5978dfb90ca8b078b19eebec178879ea9e
Mia Stevanovic <miastevanovic@Mias-MacBook-Pro.local>
2021-11-04 13:52:22 -0400

    deleted metadata files and added annotation

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/EventService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/EventService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.library.dao.EventRepository;
@@ -12,7 +13,7 @@ import ca.mcgill.ecse321.library.dao.EventRepository;
 import ca.mcgill.ecse321.library.model.Event;
 import ca.mcgill.ecse321.library.model.TimeSlot;
 import ca.mcgill.ecse321.library.model.User;
-
+@Service
 public class EventService {
 	
 	
