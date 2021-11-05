886527afda22570ed62af42ef017077e08e60f2a
Mia Stevanovic <miastevanovic@Mias-MacBook-Pro.local>
2021-11-05 15:22:04 -0400

    added annotation for TimeSlot REST controller

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/controller/TimeSlotRestController.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/controller/TimeSlotRestController.java
@@ -1,25 +1,29 @@
 package ca.mcgill.ecse321.library.controller;
 
 import java.sql.Date;
+
 import java.sql.Time;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.library.dto.TimeSlotDto;
 import ca.mcgill.ecse321.library.model.TimeSlot;
 import ca.mcgill.ecse321.library.service.TimeSlotService;
 
 
-
+@CrossOrigin(origins = "*")
+@RestController
 public class TimeSlotRestController {
 
 	
