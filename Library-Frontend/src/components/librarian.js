eb47cd2666ab6764c99bf5e00842d72a3532acc6
LuccaDiLullo <lucca.dilullo@mail.mcgill.ca>
2021-11-24 17:40:50 -0500

    librarian intro frontend

---
+++ Library-Frontend/src/components/librarian.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Router from "../router/index";
+var config = require('../../config');
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port;
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort;
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+});
+
+export default {
+    name: "librarian",
+    data(){
+        return {
+            username: localStorage.getItem('username'),
+            item_query: '',
+            item_response: [],
+            reserved_response: [],
+            error: '',
+            response: ''
+        }
+    },
+
+    methods: {
+        
+        
+    },
+};
