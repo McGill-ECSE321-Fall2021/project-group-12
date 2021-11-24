b7203a590433caae788d087c9b2fb4448c7ade18
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2021-11-23 22:59:18 -0500

    fixed logic mistake

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/LibrarianService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/LibrarianService.java
@@ -432,7 +432,7 @@ public class LibrarianService {
 						|| password.charAt(i) == 95) {
 					// special characters: !, #, $, %, &, *, +, -, =, ?, @, ^, _
 					specialChar = true;
-				} else if (!(password.charAt(i) > 96 && password.charAt(i) < 123)||!(password.charAt(i) > 47 && password.charAt(i) < 58)) {
+				} else if (!(password.charAt(i) > 96 && password.charAt(i) < 123)&&!(password.charAt(i) > 47 && password.charAt(i) < 58)) {
 					invalid = true; // may be invalid if character at i is not uppercase or one of the designated
 									// special characters.
 				}
