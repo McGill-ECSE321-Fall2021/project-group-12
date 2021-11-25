4d3dc7b438fb16c78dc4cdbe922e050c6762fe9d
LuccaDiLullo <lucca.dilullo@mail.mcgill.ca>
2021-11-24 19:50:00 -0500

    added a break before librarian

--- Library-Frontend/src/components/Librarian.vue
+++ Library-Frontend/src/components/Librarian.vue
@@ -8,6 +8,7 @@
    <div class="container">
       <div class="row d-flex justify-content-center">
         <div class="col-md-3 mt-5">
+        <br>
           <h1>Librarian</h1>
         </div>
       </div>            
