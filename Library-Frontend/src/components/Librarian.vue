76c057f0a9d24c911246925ad7a5bc6ba61cbdf0
Ann Trinh <ann.trinh@mail.mcgill.ca>
2021-11-27 16:00:27 -0500

    Frontend not tested

--- Library-Frontend/src/components/Librarian.vue
+++ Library-Frontend/src/components/Librarian.vue
@@ -14,36 +14,113 @@
             <div class="col-6">
               <div class="row d-flex justify-content-center">
                 <div class="col">
-                  <h3>Search librarian</h3>
-                  <div>
-                    <table>
-                      <tr>
-                        <td>
-                          <input class="text-field" type="text" v-model="librarianUsername" placeholder="Librarian Username">
-                        </td>
-                        <td>
-                          <button class="btn" @click="findLibrarian(librarianUsername)">Search Librarian</button>
-                        </td>
-                      </tr>
-                    </table>
+                  <h3>Remove librarian account</h3>
+                  <div class ="card-body">
+                    <input class="text-field" type="text" v-model="delId" placeholder="ID"><br>
+                    <button v-bind:disabled="!delId" @click="removeLibrarian(delId)" class="btn">Delete account</button>
                   </div>
                   <h3>Set up new librarian account</h3>
-                  <div class="card-body mt-4 mb-3">
+                  <div class="card-body">
                     <input class="text-field" type="text" v-model="firstName" placeholder="First Name"><br>
                     <input class="text-field" type="text" v-model="lastName" placeholder="Last Name"><br>
                     <input class="text-field" type="text" v-model="address" placeholder="Address"><br>
-                    <label>Local:</label><input type="checkbox" class="text-field" v-model="isLocal"><br>
                     <input class="text-field" type="text" v-model="username" placeholder="Username"><br>
                     <input class="text-field" type="text" v-model="password" placeholder="Password"><br>
                     <input class="text-field" type="text" v-model="email" placeholder="Email"><br>
-                    <label>isHead:</label><input type="checkbox" class="text-field" v-model="isHead"><br>
-                    <button v-bind:diabled="!firstName||!lastName||!address||!username||!password||!email" @click="signUpLibrarian(firstName, lastName, address, isLocal, username, password, email, isHead)" class="btn">Signup</button>
+                    <button v-bind:disabled="!firstName||!lastName||!address||!username||!password||!email" @click="signUpLibrarian(firstName, lastName, address, username, password, email)" class="btn">Signup</button>
                   </div>
                 </div>  
-              </div>    
+              </div>
+              <h3>Update account</h3>
+              <div class="card-body">
+                <input class="text-field" type="text" v-model="nwfirstName" placeholder="First Name"><br>
+                <input class="text-field" type="text" v-model="nwlastName" placeholder="Last Name"><br>
+                <input class="text-field" type="text" v-model="nwaddress" placeholder="Address"><br>
+                <input class="text-field" type="text" v-model="nwusername" placeholder="Username"><br>
+                <input class="text-field" type="text" v-model="nwemail" placeholder="Email"><br>
+                <label>isHead:</label><input type="checkbox" class="text-field" v-model="nwisHead"><br>
+                <button @click="updateLibrarian(nwfirstName, nwlastName, nwaddress, nwusername, nwemail,nwisHead)" class="btn">Save & Update</button>
+              </div>
+              <h3>Change password</h3>
+                <input class="text-field" type="text" v-model="oldPassword" placeholder="Old Password"><br>
+                <input class="text-field" type="text" v-model="newPassword" placeholder="New Password"><br>
+                <button v-bind:disabled="!oldPassword||!newPassword" @click="changePassword(oldPassword, newPassword)" class="btn">Change Password</button>   
             </div>
             <div class="col-6">
-              some filler
+              <h3>Search librarian</h3>
+                  <table>
+                    <tr>
+                      <td>
+                        <input class="text-field" type="text" v-model="librarianUsername" placeholder="Librarian Username">
+                      </td>
+                      <td>
+                        <button class="btn" @click="findLibrarian(librarianUsername)">Search</button>
+                      </td>
+                    </tr>
+                  </table>
+                  <div class="container">
+                    <div class="row align-left align-content-start">
+                      <h5>Found results:</h5><br>
+                    </div>
+                    <div class="row align-left align-content-start">
+                      <b v-if="librarian">{{ librarian.firstName }}</b>
+                      <v-divider verticle></v-divider>
+                      <b v-if="librarian">&nbsp;{{ librarian.lastName }}</b>
+                    </div>
+                    <div class="row align-left no-gutters">
+                      <b>ID:&ensp;</b>
+                      <v-divider verticle></v-divider>
+                      <p v-if="librarian">{{ librarian.id }}</p>
+                    </div>
+                    <div class="row align-left no-gutters">
+                      <b>Username:&ensp;</b>
+                      <v-divider verticle></v-divider>
+                      <p v-if="librarian">{{ librarian.username }}</p>
+                    </div>
+                    <div class="row align-left no-gutters">
+                      <b>Address:&ensp;</b>
+                      <v-divider verticle></v-divider>
+                      <p v-if="librarian">{{ librarian.address }}</p>
+                    </div>
+                    <div class="row align-left no-gutters">
+                      <b>Email:&ensp;</b>
+                      <v-divider verticle></v-divider>
+                      <p v-if="librarian">{{ librarian.email }}</p>
+                    </div>
+                  </div>
+              <h3>All librarians</h3>
+                <div>
+                  <table>
+                    <tr v-for="librarian in librarians">
+                      <div class="row align-left align-content-start">
+                        <b v-if="librarian">{{ librarian.firstName }}</b>
+                        <v-divider verticle></v-divider>
+                        <b v-if="librarian">&nbsp;{{ librarian.lastName }}</b>
+                      </div>
+                      <div class="row align-left no-gutters">
+                        <b>ID:&ensp;</b>
+                        <v-divider verticle></v-divider>
+                        <p v-if="librarian">{{ librarian.id }}</p>
+                      </div>
+                      <div class="row align-left no-gutters">
+                        <b>Username:&ensp;</b>
+                        <v-divider verticle></v-divider>
+                        <p v-if="librarian">{{ librarian.username }}</p>
+                      </div>
+                      <div class="row align-left no-gutters">
+                        <b>Address:&ensp;</b>
+                        <v-divider verticle></v-divider>
+                        <p v-if="librarian">{{ librarian.address }}</p>
+                      </div>
+                      <div class="row align-left no-gutters">
+                        <b>Email:&ensp;</b>
+                        <v-divider verticle></v-divider>
+                        <p v-if="librarian">{{ librarian.email }}</p>
+                      </div>
+                    </tr>
+                  </table>
+                  <button class="btn" @click="findAllLibrarians()">Refresh</button>
+                </div>
             </div>
           </div>
         </div>  
@@ -69,7 +146,6 @@
     font-family: var(--font);
     color: var(--light);
     background: var(--dark-color);
-    height: 100vh;
   }
   .navbar-main {
     font-family: var(--font);
--- Library-Frontend/src/components/librarian.js
+++ Library-Frontend/src/components/librarian.js
@@ -10,7 +10,6 @@ var AXIOS = axios.create({
   headers: { 'Access-Control-Allow-Origin': frontendUrl }
 });
 
-//would this just be imported?
 function LibrarianDto (firstName, lastName, address, isLocal, username, password, email, isHead, id) {
     this.firstName = firstName
     this.lastName = lastName
@@ -21,6 +20,9 @@ function LibrarianDto (firstName, lastName, address, isLocal, username, password
     this.email = email
     this.isHead = isHead
     this.id = id
+}
+function EventDto () {
+
 }
 
 export default {
@@ -33,13 +35,59 @@ export default {
             reserved_response: [],
             error: '',
             response: '',
-            librarian: ''
+            librarian: '',
+            librarians: []
         }
+    },
+    
+    created: function() {
+        const lib = new LibrarianDto('Bob', 'Ross', '123 street', true, 'BobisRoss', 'absCdc!', 'bob@ross.com', true, 2)
+        this.librarian = lib
+        const lib2 = new LibrarianDto('Van', 'Gogh', '124 street', true, 'VanisGogh', 'absCdc!', 'van@gogh.com', true, 2)
+        this.librarians = [lib, lib2]
+
     },
 
     methods: {
         findLibrarian: function (librarianUsername) {
-            AXIOS.get('/librarian/', librarianUsername)
+            AXIOS.get('/librarian/'+librarianUsername)
+            .then(response => {
+                this.response = response.data;
+                this.librarian = response.data;
+            })
+            .catch(e => {
+                console.log('frontend url: ' + frontendUrl)
+                console.log('\nbackend url:' + backendUrl)
+                this.error = e;
+            })
+        },
+        signUpLibrarian: function (firstName, lastName, address, username, password, email) {
+            AXIOS.post('librarianh/create/head?firstName='+firstName+'&lastName='+lastName+'&address='+address+'&email='+email+'&password='+password+'&username='+username)
+            .then(response => {
+                this.response = response.data;
+                localStorage.setItem('username', username);
+                this.librarians.push(response.data);
+            })
+            .catch(e => {
+                console.log('frontend url: ' + frontendUrl)
+                console.log('\nbackend url:' + backendUrl)
+                this.error = e;
+            })
+        },
+        findAllLibrarians: function () {
+            AXIOS.get('/librarians/')
+            .then(response => {
+                this.response = response.data;
+                this.librarians.push(response);
+            })
+            .catch(e => {
+                console.log('frontend url: ' + frontendUrl)
+                console.log('\nbackend url:' + backendUrl)
+                this.error = e;
+            })
+        },
+        removeLibrarian: function (delId) {
+            AXIOS.get('/librarians/delete/'+delId+'?librarianUsername='+localStorage.getItem('username'))
             .then(response => {
                 this.response = response.data;
             })
@@ -49,11 +97,22 @@ export default {
                 this.error = e;
             })
         },
-        signUpLibrarian: function (firstName, lastName, address, isLocal, username, password, email, isHead) {
-            AXIOS.post('librarian/create?firstName='+firstName+'&lastName='+lastName+'&address='+address+'&isLocal='+isLocal+'&username='+username+'&password='+password+'&email='+email+'&isHead='+isHead)
+        updateLibrarian: function (nwfirstName, nwlastName, nwaddress, nwusername, nwemail, nwIsHead) {
+            AXIOS.get('/librarians/update/'+localStorage.getItem('username')+'?firstname='+nwfirstName+'&lastName='+nwlastName+
+            '&address='+nwaddress+'&email='+nwemail+'&password='+librarian.password+'&username='+nwusername+'&isHead='+nwIsHead)
             .then(response => {
                 this.response = response.data;
-                localStorage.setItem('username', username);
+            })
+            .catch(e => {
+                console.log('frontend url: ' + frontendUrl)
+                console.log('\nbackend url:' + backendUrl)
+                this.error = e;
+            })
+        },
+        changePassword: function (oldPassword, newPassword) {
+            AXIOS.get('/librarians/change-password/?username='+localStorage.getItem('username')+'&oldPassword='+oldPassword+'&newPassword='+newPassword)
+            .then(response => {
+                this.response = response.data;
             })
             .catch(e => {
                 console.log('frontend url: ' + frontendUrl)
