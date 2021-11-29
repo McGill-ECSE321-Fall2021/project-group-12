9e09508afb5f396fe3805ac9aa7ac62a1ac8b370
Chiragsud <92189393+Chiragsud@users.noreply.github.com>
2021-11-29 18:42:12 +0300

    Update OnlineUser.vue
    added deleteonlineuser button

--- Library-Frontend/src/components/OnlineUser.vue
+++ Library-Frontend/src/components/OnlineUser.vue
@@ -14,6 +14,9 @@
       <div class="row d-flex justify-content-center">
         <div class="col-xl-12 mt-3 mb-3">
             <button @click="logout()" class="btn">Logout</button>
+            <button @click="deleteOnlineUser()" class="btn">
+              Delete Online User
+            </button>
         </div>
     </div>          
    </div>
